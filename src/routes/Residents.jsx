8977efde02747a09a6e385a848d9f2f3c2efec3f
dagopia <tdaguti@gmail.com>
2023-02-02 07:43:46 -0800

    private fc

--- src/routes/Residents.jsx
+++ src/routes/Residents.jsx
@@ -6,7 +6,7 @@ import { useTranslation } from "react-i18next";
 const PageContent = (props) => {
   return (
     <Box>
-      <h1>{props.pageName}</h1>
+      <h1>{props.pageName} fc</h1>
     </Box>
   );
 };
