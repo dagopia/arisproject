e6c4ff97a9521c620f48ab925bd3523441b3e0e3
ethiobek <arkishasoftware@gmail.com>
2023-02-02 16:11:06 +0400

    start implementing the dashcard component to the dashboard route

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,14 +1,98 @@
-import { Group } from "@mui/icons-material";
-import { Box } from "@mui/material";
+import {
+  Add,
+  Group,
+  Groups,
+  Hail,
+  HouseRounded,
+  PersonAdd,
+} from "@mui/icons-material";
+import { Box, Grid } from "@mui/material";
 import { Container } from "../components/Container";
 import { useTranslation } from "react-i18next";
+import { DashCard } from "../components/Cards";
+import { BlueActive } from "../utils/colors";
 
 const PageContent = () => {
   const { t } = useTranslation();
-
   return (
-    <Box>
-      <h1>{t("dashboard")}</h1>
+    <Box sx={{ mt: 2 }}>
+      <Grid container spacing={2}>
+        <Grid xs={12} md={6} lg={3} sm={12}>
+          <DashCard
+            icon={
+              <Groups
+                sx={{
+                  width: "80px",
+                  fontSize: "75px",
+                  pr: 3,
+                  color: BlueActive,
+                }}
+              />
+            }
+            title={t("total_residents")}
+            value={"4,451,525"}
+            footerText={`400 ${t("today")}`}
+            startIcon={<Add />}
+            fontSize={"h5"}
+          />
+        </Grid>
+        <Grid xs={12} md={6} lg={3} sm={12}>
+          <DashCard
+            icon={
+              <HouseRounded
+                sx={{
+                  width: "80px",
+                  fontSize: "75px",
+                  pr: 3,
+                  color: BlueActive,
+                }}
+              />
+            }
+            title={t("total_house_owners")}
+            value={"4,451,525"}
+            footerText={`400 ${t("today")}`}
+            startIcon={<Add />}
+            fontSize={"h5"}
+          />
+        </Grid>
+        <Grid xs={12} md={6} lg={3} sm={12}>
+          <DashCard
+            icon={
+              <Hail
+                sx={{
+                  width: "80px",
+                  fontSize: "75px",
+                  pr: 3,
+                  color: BlueActive,
+                }}
+              />
+            }
+            title={t("new_comers_reg")}
+            value={"4,448"}
+            footerText={`400 ${t("today")}`}
+            startIcon={<Add />}
+            fontSize={"h5"}
+          />
+        </Grid>
+        <Grid xs={12} md={6} lg={3} sm={12}>
+          <DashCard
+            icon={
+              <PersonAdd
+                sx={{
+                  width: "80px",
+                  fontSize: "75px",
+                  pr: 3,
+                  color: BlueActive,
+                }}
+              />
+            }
+            title={t("total_today_reg")}
+            value={"4,451,525"}
+            startIcon={<Add />}
+            fontSize={"h5"}
+          />
+        </Grid>
+      </Grid>
     </Box>
   );
 };
@@ -18,7 +102,7 @@ export const Dashboard = () => {
 
   return (
     <Container
-      content={<PageContent />}
+      content={<PageContent pageName={t("dashboard")} />}
       icon={<Group />}
       pageName={t("dashboard")}
     />
